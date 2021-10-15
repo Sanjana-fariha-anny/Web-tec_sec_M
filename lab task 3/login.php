a2656a52920385be4b24ba7e250ee2471ea7d0bf
Sanjana-fariha-anny <91148461+Sanjana-fariha-anny@users.noreply.github.com>
2021-10-15 19:20:23 +0600

    lab task 3

---
+++ lab task 3/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<body>
+    
+     <form action="">
+     <fieldset>
+              <legend>Registration</legend>
+     <br>
+     
+     <div>
+         <label for="UserName">UserName</label>
+         <input type="text" name="UserName" placeholder="U_Name">
+
+      </div>
+
+      <br>
+      <div>
+           <label for="Password">Password</label>
+           <input type="number" value="" placeholder="Password">
+      </div>
+     <hr>
+     <div>
+     <input type="checkbox"name="Remember Me">Remember Me
+     </div>
+     <br>
+       <div>
+       <input type="submit" name="submit">
+       
+       <a href="#">Forget Password?</a>
+
+       </div>
+       </fieldset>
+      </form>
+</body>
+</html>
---
+++ lab task 3/password.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<body>
+    
+    <form action="">
+    <fieldset>
+              <legend>Change Password</legend>
+    <br>
+    
+    
+    <div>
+         <label for="CureentPassword">CureentPassword</label>
+         <input type="text" name="CurrentPassword"value="">
+
+    </div>
+<br>
+    <div>
+         <label for="NewPassword">NewPassword</label>
+         <input type="text" name="NewPassword"value="">
+
+    </div>
+    <br>
+    <div>
+         <label for=" Retype New Password"> Retype NewPassword</label>
+         <input type="text" name="Retype New Password"value="">
+
+    </div>
+
+    <hr>
+    <div>
+                <input type="submit" name="submit">
+             
+             
+             </div>
+   
+
+
+    </fieldset>
+    </form>
+</body>
+</html>
